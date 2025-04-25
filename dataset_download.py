aa9d3ebcbd290937fddf0ad3630a90cc0d42ab0c
zhanyifeng <140321694+ZYF7189@users.noreply.github.com>
2025-04-25 21:07:45 +0800

    Create dataset_download.py

---
+++ dataset_download.py
@@ -0,0 +1,7 @@
+## 数据集1：CIFAR10
+这个数据集可以在运行 data_preprocess.py 时自动下载，只需要在主程序中运行 get_data_cifar10() 函数
+## 数据集2：flowers
+这个数据集的下载链接如下：  
+http://download.tensorflow.org/example_images/flower_photos.tgz  
+下载后需要将文件目录修改成 ./flower_data/flower_photos/五个花文件夹 的形式
+此外对于这个花数据集的预处理方法借鉴了该篇文章https://blog.csdn.net/m0_37867091/article/details/107150142
